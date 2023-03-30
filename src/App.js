26038186a5e282cfb1d64840b2a06f0c978e7eef
Chirag Sharma <chiragsharma75757@gmail.com>
2023-03-30 20:19:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./components/form/Form.js";
+import Message from "./components/message/Message.js";
+import "./style.css";
+import React, { useState } from "react";
 
 function App() {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  const handleSubscription = (isSubscribed) => {
+    setIsSubscribed(isSubscribed);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Message isSubscribed={isSubscribed} />
+      <Form onSubscribe={handleSubscription} />
+    </>
   );
 }
 
